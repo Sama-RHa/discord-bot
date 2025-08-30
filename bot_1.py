a5e652b1c5e100294eb950bebef1aafe93a22d92
Sama-RHa <samarhadilone@gmail.com>
2025-08-30 15:22:47 -0400

    Create bot_1.py

---
+++ bot_1.py
@@ -0,0 +1,52 @@
+import discord
+import random
+
+# La variable intents almacena los privilegios del bot
+intents = discord.Intents.default()
+# Activar el privilegio de lectura de mensajes
+intents.message_content = True
+# Crear un bot en la variable cliente y transferirle los privilegios
+client = discord.Client(intents=intents)
+
+def gen_pass(pass_length):
+    elements = "+-/*!&$#?=@<>"
+    password = ""
+
+    for i in range(pass_length):
+        password += random.choice(elements)
+
+    return password
+
+def gen_emodji():
+    emodji = ["\U0001f600", "\U0001f642", "\U0001F606", "\U0001F923"]
+    return random.choice(emodji)
+
+
+def flip_coin():
+    flip = random.randint(1, 2)
+    if flip == 1:
+        return "You rolled heads!"
+    else:
+        return "You rolled tails!"
+
+
+@client.event
+async def on_ready():
+    print(f'Hemos iniciado sesión como {client.user}')
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+    if message.content.startswith('$hello'):
+        await message.channel.send("Hi!")
+    elif message.content.startswith('$bye'):
+        await message.channel.send("\U0001f642")
+    elif message.content.startswith('$password'):
+        await message.channel.send(gen_pass(10))
+    elif message.content.startswith('$emodji'):
+        await message.channel.send(gen_emodji())
+    elif message.content.startswith('$flip'):
+        await message.channel.send(flip_coin())
+
+client.run("Token goes here")
